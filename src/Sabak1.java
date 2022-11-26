a449586af889a893b607efef3c7dda29f95ec40b
Danik <dan_ntu@mail.ru>
2022-11-26 22:40:08 +0600

    Initial commit

---
+++ src/Sabak1.java
@@ -0,0 +1,21 @@
+public class Sabak1 {
+
+
+    public static void main(String[] args) {
+
+        int height = 2;
+        int width = 5;
+
+        byte b  =  127;
+
+        int rectangleArea = height*width;
+
+        System.out.println(rectangleArea);
+        System.out.println(rectangleArea);
+
+
+
+    }
+
+
+}
